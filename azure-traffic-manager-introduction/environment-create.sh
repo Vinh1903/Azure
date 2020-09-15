b3d7558c782c4e828f5bc114b4aa8f0ffd82198b
Vinh1903 <42708908+Vinh1903@users.noreply.github.com>
2020-09-16 06:27:51 +0700

    Update environment-create.sh

--- azure-traffic-manager-introduction/environment-create.sh
+++ azure-traffic-manager-introduction/environment-create.sh
@@ -1,16 +1,16 @@
 group=azure-traffic-manager-introduction
-az group create -g $group -l northeurope
+az group create -g $group -l eastasia
 username=adminuser
-password='SecretPassword123!@#'
+password = 'SecretPassword123!@#'
 az vm create \
-  -n vm-northeurope \
+  -n vm-eastasia \
   -g $group \
-  -l northeurope \
+  -l eastasia \
   --image Win2019Datacenter \
   --admin-username $username \
   --admin-password $password \
   --nsg-rule rdp
-  
+
 az vm create \
   -n vm-eastus2 \
   -g $group \
@@ -25,25 +25,25 @@ az appservice plan create \
   -g $group \
   -l eastus2 \
   --sku S1
-  
+
 appname=demo-web-eastus2-$RANDOM$RANDOM
 az webapp create \
   -n $appname \
   -g $group \
   -p web-eastus2-plan
-  
+
 az appservice plan create \
-  -n web-northeurope-plan \
+  -n web-eastasia-plan \
   -g $group \
-  -l northeurope \
+  -l eastasia \
   --sku S1
-  
-appname=demo-web-northeurope-$RANDOM$RANDOM
+
+appname=demo-web-eastasia-$RANDOM$RANDOM
 az webapp create \
   -n $appname \
   -g $group \
-  -p web-northeurope-plan
-  
+  -p web-eastasia-plan
+
 az webapp list -g $group --query "[].enabledHostNames" -o jsonc
 
 az vm list \
